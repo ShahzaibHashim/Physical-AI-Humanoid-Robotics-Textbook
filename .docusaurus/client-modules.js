f6b114eb528bbdc8d26f44d184e83d4fd20f6165
ShahzaibHashim <shahzaib11junejo@gmail.com>
2026-01-11 19:38:37 +0500

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("E:\\My Project\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\infima\\dist\\css\\default\\default-rtl.css"),
+  require("E:\\My Project\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("E:\\My Project\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("E:\\My Project\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\src\\css\\custom.css"),
+];
